ddce787804c099824671d3e32baa528d03aa6419
daaappssszz10-prog <daaappssszz10@gmail.com>
2025-08-04 22:22:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function tampilkanPesan() {
+  alert("Aku bersyukur punya kamu. Semoga monthversary ini jadi awal dari bulan-bulan bahagia berikutnya ❤");
+}
+function playMusic() {
+  document.getElementById("musik").play();
+}
+
+function pauseMusic() {
+  document.getElementById("musik").pause();
+}
